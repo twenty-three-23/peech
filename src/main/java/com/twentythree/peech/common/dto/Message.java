f0e20123d609672b531198f9a0ea4a1ccf4fad78
snacktime <tth8181@gmail.com>
2024-06-20 23:35:48 +0900

    feat: TT-84 GPT DTO 생성
    GPT와 통신을 위한 request, response dto 생성

---
+++ src/main/java/com/twentythree/peech/common/dto/Message.java
@@ -0,0 +1,12 @@
+package com.twentythree.peech.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Message {
+    private String role;
+    private String content;
+
+}
