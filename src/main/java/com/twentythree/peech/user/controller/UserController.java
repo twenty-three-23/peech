d77abb1a4923d8d54bf6cbdf230aea623e973a86
snacktime <tth8181@gmail.com>
2024-06-20 17:19:04 +0900

    feat: TT-69 userController 구현
    유저 가입(createuser) 컨트롤러 구현
    path는 api/v1/user
    method는 post

---
+++ src/main/java/com/twentythree/peech/user/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.twentythree.peech.user.controller;
+
+import com.twentythree.peech.user.dto.request.CreateUserRequestDTO;
+import com.twentythree.peech.user.dto.response.CreateUserResponseDTO;
+import com.twentythree.peech.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController implements SwaggerUserController{
+
+    private final UserService userService;
+
+    @Operation(summary = "유저 가입",
+            description = "deviceId를 CreateUserRequestDTO에 담아 요청하면 생성된 UserId를 CreteUserResponseDTO에 담아 응답한다.")
+    @Override
+    @PostMapping("api/v1/user")
+    public CreateUserResponseDTO createUser(@RequestBody CreateUserRequestDTO request) {
+        Long userId = userService.createUser(request);
+        return new CreateUserResponseDTO(userId);
+    }
+}
