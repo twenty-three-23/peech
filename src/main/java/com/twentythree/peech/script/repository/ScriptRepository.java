6786a5104a9a58545d6f6cd2406d601d6424dab1
snacktime <tth8181@gmail.com>
2024-06-21 00:18:58 +0900

    feat: ScriptRepository 구현
    JpaRepository를 이용해 ScriptRepository 구현

---
+++ src/main/java/com/twentythree/peech/script/repository/ScriptRepository.java
@@ -0,0 +1,7 @@
+package com.twentythree.peech.script.repository;
+
+import com.twentythree.peech.script.domain.ScriptEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScriptRepository extends JpaRepository<ScriptEntity, Long> {
+}
