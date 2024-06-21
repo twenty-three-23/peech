7e0d8a4a6812b676e9bdeba85784f50131ef121a
snacktime <tth8181@gmail.com>
2024-06-21 21:09:30 +0900

    feat: TT-92 대본 입력시 문단으로 분해
    GPT를 이용하여 대본을 문단으로 분리

--- src/main/java/com/twentythree/peech/script/service/ScriptService.java
+++ src/main/java/com/twentythree/peech/script/service/ScriptService.java
@@ -1,16 +1,15 @@
 package com.twentythree.peech.script.service;
 
+import com.twentythree.peech.common.dto.request.GPTRequest;
+import com.twentythree.peech.common.dto.response.GPTResponse;
 import com.twentythree.peech.script.domain.InputAndSttType;
 import com.twentythree.peech.script.domain.ScriptEntity;
 import com.twentythree.peech.script.domain.VersionPk;
-import com.twentythree.peech.script.dto.response.ProcessedScriptResponseDTO;
 import com.twentythree.peech.script.repository.ScriptRepository;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.resource.beans.container.spi.BeanLifecycleStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalTime;
 
@@ -18,13 +17,20 @@ import java.time.LocalTime;
 @Service
 public class ScriptService {
 
+    @Value("${gpt.model}")
+    private String model;
+
+    @Value("${gpt.api.url}")
+    private String apiURL;
+    private final RestTemplate restTemplate;
     private final ScriptRepository scriptRepository;
 
-    @Autowired
-    public ScriptService(ScriptRepository scriptRepository) {
-        this.scriptRepository = scriptRepository;
-    }
 
+    @Autowired
+    public ScriptService(ScriptRepository scriptRepository, RestTemplate restTemplate) {
+        this.scriptRepository = scriptRepository;
+        this.restTemplate = restTemplate;
+    }
 
 
     public ScriptEntity saveInputScript(Long packageId, String text) {
@@ -43,12 +49,17 @@ public class ScriptService {
 
         ScriptEntity scriptEntity = ScriptEntity.ofCreateInputScript(versionPk, text, expectedTime, InputAndSttType.INPUT);
 
+
+
         scriptRepository.save(scriptEntity);
         return scriptEntity;
     }
 
+    public String[] saveScriptToParagraph(String text) {
+        String[] paragraphs = sliceScriptToParagraph(text);
 
 
+    }
 
 
     // 메소드
@@ -69,14 +80,28 @@ public class ScriptService {
 
     private LocalTime transferSeoondToLocalTime(float time) {
 
-        int second = (int)time;
+        int second = (int) time;
 
-        int minute = (int) (second/60);
+        int minute = (int) (second / 60);
         int hour = (int) (second / (60 * 60));
 
         int minuteSet = minute - (hour * 60);
         int secondSet = second - (minuteSet * 60);
 
         return LocalTime.of(hour, minuteSet, secondSet);
+    }
+
+
+    public String[] sliceScriptToParagraph(String text){
+
+        String prompt = "아래의 스크립트를 문장으로 나누고 그 문장들을 문단으로 다시 합쳐줘 " + text;
+
+        GPTRequest request = new GPTRequest(model, prompt);
+        GPTResponse Response = restTemplate.postForObject(apiURL, request, GPTResponse.class);
+        String result = Response.getChoices().get(0).getMessage().getContent();
+
+        String[] paragraphs = result.split("\n");
+
+        return paragraphs;
     }
 }
