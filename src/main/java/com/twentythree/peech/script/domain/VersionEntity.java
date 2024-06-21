f1cae99aef892fb23bef425bbec619e8d0b99143
snacktime <tth8181@gmail.com>
2024-06-21 18:08:27 +0900

    refactor: TT-89 version entity 리팩토링
    column 들을 snake case로 변경
    table 이름을 version으로 변경

--- src/main/java/com/twentythree/peech/script/domain/VersionEntity.java
+++ src/main/java/com/twentythree/peech/script/domain/VersionEntity.java
@@ -11,17 +11,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @IdClass(VersionPk.class)
+@Table(name = "VERSION")
 @Entity
 public class VersionEntity extends BaseCreatedAtEntity {
 
     @Id
+    @Column(name = "major_version")
     private Long majorVersion;
     @Id
+    @Column(name = "minor_version")
     private Long minorVersion;
 
     // @ManyToOne(fetch = FetchType.LAZY) package entity가 완성되면 packageId에 @OneToMany로 연결
     @JoinColumn
-    @Column(nullable = false)
+    @Column(name = "package_id", nullable = false)
     private Long packageId;
 
     public static VersionEntity of(Long majorVersion, Long minorVersion, Long packageId) {
