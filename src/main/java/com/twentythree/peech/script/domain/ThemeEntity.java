6f401d025460e9a65c2f923994d15c914a27fff6
snacktime <tth8181@gmail.com>
2024-06-25 22:36:36 +0900

    feat: TT-145 ThemeEntity 생성 팩토리 메소드 만들기

--- src/main/java/com/twentythree/peech/script/domain/ThemeEntity.java
+++ src/main/java/com/twentythree/peech/script/domain/ThemeEntity.java
@@ -2,15 +2,19 @@ package com.twentythree.peech.script.domain;
 
 
 import com.twentythree.peech.common.domain.BaseTimeEntity;
+import com.twentythree.peech.script.repository.ThemeRepository;
 import com.twentythree.peech.user.domain.UserEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.catalina.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @Entity
 @Table(name = "THEME")
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -31,6 +35,15 @@ public class ThemeEntity extends BaseTimeEntity {
 
     @OneToMany(mappedBy = "ThemeEntity")
     private List<VersionEntity> versions = new ArrayList<>();
+
+    private ThemeEntity(UserEntity userEntity, String themeTitle) {
+        this.userEntity = userEntity;
+        this.themeTitle = themeTitle;
+    }
+
+    public static ThemeEntity of(UserEntity userEntity, String themeTitle) {
+        return new ThemeEntity(userEntity, themeTitle);
+    }
 
 }
 
