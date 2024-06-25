10de1f24f5849b2b5b43a79fda9b9878b42c6e03
snacktime <tth8181@gmail.com>
2024-06-25 14:59:23 +0900

    feat: TT-132 대본 및 문장 저장 controller 구현
    packageId와 대본을 response body에 입력받아 scriptSentenceFacade의 createScriptAndSentence를 이용해 대본과 문장들을 모두 저장하고 버전생성 후 scriptId를 반환

--- src/main/java/com/twentythree/peech/script/controller/SwaggerScriptInterface.java
+++ src/main/java/com/twentythree/peech/script/controller/SwaggerScriptInterface.java
@@ -1,8 +1,11 @@
 package com.twentythree.peech.script.controller;
 
+import com.twentythree.peech.script.dto.request.ParagraphsRequestDTO;
+import com.twentythree.peech.script.dto.request.ScriptIdRequestDTO;
 import com.twentythree.peech.script.dto.request.ScriptRequestDTO;
-import com.twentythree.peech.script.dto.response.ProcessedScriptResponseDTO;
-import io.swagger.v3.oas.annotations.Operation;
+import com.twentythree.peech.script.dto.SaveScripDTO;
+import com.twentythree.peech.script.dto.response.ExpectedTimeResponseDTO;
+import com.twentythree.peech.script.dto.response.SaveScriptAndSentenceResponseDTO;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -10,9 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 public interface SwaggerScriptInterface {
 
-
-    @Operation(summary = "대본 입력", description = "대본 전송시 문단으로 나눠서 응답")
-    @ApiResponse(responseCode = "200", description = "성공", content = {@Content(schema = @Schema(implementation = ProcessedScriptResponseDTO.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "201", description = "성공", content = {@Content(schema = @Schema(implementation = SaveScriptAndSentenceResponseDTO.class), mediaType = "application/json")})
     @ApiResponse(responseCode = "400", description = "실패", content = {@Content(schema = @Schema(implementation = Error.class), mediaType = "application/json")})
-    ProcessedScriptResponseDTO processScript(@RequestBody ScriptRequestDTO paragraph);
+    SaveScriptAndSentenceResponseDTO saveScriptAndSentence(@RequestBody ParagraphsRequestDTO request);
+
 }
---
+++ src/main/java/com/twentythree/peech/script/dto/request/ParagraphsRequestDTO.java
@@ -0,0 +1,5 @@
+package com.twentythree.peech.script.dto.request;
+
+
+public record ParagraphsRequestDTO(Long packageId, String[] paragraphs) {
+}
