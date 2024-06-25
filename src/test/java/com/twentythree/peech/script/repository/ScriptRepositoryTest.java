3f6f39a91f5dab48cd7e6c0b69feb3219f6772e8
snacktime <tth8181@gmail.com>
2024-06-25 15:08:43 +0900

    test: TT-135 ScriptRepository의 findByMaxMajorVersionInPackageId 테스트

---
+++ src/test/java/com/twentythree/peech/script/repository/ScriptRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.twentythree.peech.script.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class ScriptRepositoryTest {
+
+    @Autowired
+    private ScriptRepository scriptRepository;
+
+    @Test
+    public void 패키지_아이디에서_가장_최신의_major_version_찾기() throws Exception {
+        // given
+
+        // when
+        Long id = scriptRepository.findByMaxMajorVersionInPackageId(1L);
+
+        // then
+        assertThat(id).isEqualTo(3L);
+
+    }
+
+
+}
