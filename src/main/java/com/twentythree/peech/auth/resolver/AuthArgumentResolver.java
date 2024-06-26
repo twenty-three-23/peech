0a19ab054fb2030610646dda2c8a33bee5ebba3c
snacktime <tth8181@gmail.com>
2024-06-26 14:09:32 +0900

    fix: TT-155 쿠키에서 userId가져오는 로직 재구현
    HttpServletRequest에서 cookies를 가져오고 순회하면서 userId키 값을 가진 쿠키에서 userId를 가져온다.

--- src/main/java/com/twentythree/peech/auth/resolver/AuthArgumentResolver.java
+++ src/main/java/com/twentythree/peech/auth/resolver/AuthArgumentResolver.java
@@ -2,13 +2,16 @@ package com.twentythree.peech.auth.resolver;
 
 import com.twentythree.peech.auth.dto.LoginUserId;
 import com.twentythree.peech.auth.dto.UserIdDTO;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
@@ -20,7 +23,22 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
 
-        Long userId = Long.parseLong(request.getParameter("userId"));
+        Cookie[] cookies = request.getCookies();
+        log.info("1");
+        if (cookies == null) {
+            throw new IllegalArgumentException("로그인을 다시 해주세요");
+        }
+        log.info("{}", cookies);
+
+        Long userId = null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("userId")) {
+                userId = Long.parseLong(cookie.getValue());
+            }
+        }
+
+        if (userId == null) {throw new IllegalArgumentException("cookie의 userId가 잘 못 되었습니다");}
+
         return new UserIdDTO(userId);
     }
 }
