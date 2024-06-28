bda83049ba9606207384c32187a772dc12535a62
snacktime <tth8181@gmail.com>
2024-06-28 22:55:04 +0900

    feat: TT-161 userId를 토큰에 담아 응답
    secretKey를 환경변수로 설정 후 base64로 인코딩해서 jwt를 생성 jwt의 payload에는 "sub": "LoginToken", "userId":{userId}가 생성된다.

--- src/main/java/com/twentythree/peech/user/controller/SwaggerUserController.java
+++ src/main/java/com/twentythree/peech/user/controller/SwaggerUserController.java
@@ -6,9 +6,10 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface SwaggerUserController {
     @ApiResponse(responseCode = "201", description = "성공", content = {@Content(schema = @Schema(implementation = CreateUserResponseDTO.class), mediaType = "application/json")})
     @ApiResponse(responseCode = "400", description = "실패", content = {@Content(schema = @Schema(implementation = Error.class), mediaType = "application/json")})
-    CreateUserResponseDTO createUser(@RequestBody CreateUserRequestDTO request);
+    CreateUserResponseDTO createUser(@RequestParam String deviceId);
 }
