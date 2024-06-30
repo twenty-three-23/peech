aeca63af1c98410d136fb1b60db4ced5eeec6c9b
snacktime <tth8181@gmail.com>
2024-06-30 15:58:08 +0900

    feat: TT-162 token에서 userId가져오기
    parseJWT에 토큰을 넣으면 jwt를 해독해 전달한다.
    쿠키에서 꺼낸 jwt토큰을 parseJWT에서 해독해 userId를 꺼내 전달한다.

--- src/main/java/com/twentythree/peech/auth/config/ArgumentResolverConfig.java
+++ src/main/java/com/twentythree/peech/auth/config/ArgumentResolverConfig.java
@@ -1,16 +1,22 @@
 package com.twentythree.peech.auth.config;
 
 import com.twentythree.peech.auth.resolver.AuthArgumentResolver;
+import com.twentythree.peech.common.utils.JWTUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Configuration
 public class ArgumentResolverConfig implements WebMvcConfigurer {
+
+    private final JWTUtils jwtUtils;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new AuthArgumentResolver());
+        resolvers.add(new AuthArgumentResolver(jwtUtils));
     }
 }
