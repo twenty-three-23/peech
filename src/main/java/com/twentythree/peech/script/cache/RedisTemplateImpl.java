7cf4c8788cec7cd04d9e35360b2dea060ebfdd29
ch8930 <ch8930@naver.com>
2024-07-10 17:17:16 +0900

    feat: TT-222 Redis 조회로직 추가 + 테스트 코드 수정 Redis에 저장한 데이터 조회하는 기능 추가

--- src/main/java/com/twentythree/peech/script/cache/RedisTemplateImpl.java
+++ src/main/java/com/twentythree/peech/script/cache/RedisTemplateImpl.java
@@ -1,7 +1,6 @@
 package com.twentythree.peech.script.cache;
 
 import com.twentythree.peech.script.dto.RedisSentenceDTO;
-import com.twentythree.peech.script.dto.SaveRedisSentenceInfoDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,19 +21,19 @@ public class RedisTemplateImpl implements CacheService {
     private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
-    public void saveSentencesIdList(String userId, List<Long> sentencesIdList){
+    public void saveSentencesIdList(String userKey, List<Long> sentencesIdList){
 
         try{
             // Long 타입의 SentenceId List를 String 타입으로 변환
             List<String> sentenceIdListLongToString = sentencesIdList.stream().map(String::valueOf).toList();
 
             for(String sentenceId : sentenceIdListLongToString) {
-                redisTemplate.opsForList().rightPush(userId, sentenceId);
+                redisTemplate.opsForList().rightPush(userKey, sentenceId);
             }
-            log.info("Successfully saved sentenceID List for user: {}", userId);
+            log.info("Successfully saved sentenceID List for user: {}", userKey);
         } catch (Exception e) {
-            log.error("Error saving sentenceID List for user: {}", userId, e);
-            throw new RuntimeException("Error saving sentenceID List for user: " + userId);
+            log.error("Error saving sentenceID List for user: {}", userKey, e);
+            throw new RuntimeException("Error saving sentenceID List for user: " + userKey);
         }
     }
 
@@ -41,16 +41,16 @@ public class RedisTemplateImpl implements CacheService {
     public void saveSentenceInfo(Long sentenceId, RedisSentenceDTO redisSentence){
 
         try {
-                Map<String, String> sentenceInfomations = new HashMap<>();
-                sentenceInfomations.put("paragraphId", redisSentence.getParagraphId().toString());
-                sentenceInfomations.put("paragraphOrder", redisSentence.getParagraphOrder().toString());
-                sentenceInfomations.put("sentenceContent", redisSentence.getSentenceContent());
-                sentenceInfomations.put("sentenceOrder", redisSentence.getSentenceOrder().toString());
-                sentenceInfomations.put("sentenceTime", redisSentence.getTime().toString());
-                sentenceInfomations.put("isChanged", redisSentence.toStringIsChanged());
+                Map<String, String> sentenceInformations = new HashMap<>();
+                sentenceInformations.put("paragraphId", redisSentence.getParagraphId().toString());
+                sentenceInformations.put("paragraphOrder", redisSentence.getParagraphOrder().toString());
+                sentenceInformations.put("sentenceContent", redisSentence.getSentenceContent());
+                sentenceInformations.put("sentenceOrder", redisSentence.getSentenceOrder().toString());
+                sentenceInformations.put("sentenceTime", redisSentence.getTime().toString());
+                sentenceInformations.put("isChanged", redisSentence.toStringIsChanged());
 
                 // 해당 문장의 정보를 담아주는 Hash 저장
-                redisTemplate.opsForHash().putAll(sentenceId.toString(), sentenceInfomations);
+                redisTemplate.opsForHash().putAll(sentenceId.toString(), sentenceInformations);
                 log.info("Successfully saved redisSentence: {}", redisSentence);
         }catch (Exception e) {
             log.error("Error saving redisSentence List: {}",sentenceId,e);
@@ -60,12 +60,27 @@ public class RedisTemplateImpl implements CacheService {
 
     @Override
     public List<Long> findAllByUserKey(String userKey) {
-        return List.of();
+
+        List<Long> list = redisTemplate.opsForList().range(userKey, 0, -1).stream()
+                .map(String::valueOf)
+                .map(Long::parseLong)
+                .toList();
+
+        List<Long> sentenceIdsList = Optional.ofNullable(list).orElseThrow( () -> new RuntimeException("해당 유저에 대한 대본이 존재하지 않습니다.: " + userKey));
+
+        return sentenceIdsList;
     }
 
     @Override
     public RedisSentenceDTO findByKey(Long sentenceId) {
-        return null;
+        Map<Object, Object> sentenceInformation = redisTemplate.opsForHash().entries(sentenceId.toString());
+
+        return new RedisSentenceDTO(sentenceInformation.get("paragraphId").toString(),
+                sentenceInformation.get("paragraphOrder").toString(),
+                sentenceInformation.get("sentenceOrder").toString(),
+                sentenceInformation.get("sentenceContent").toString(),
+                sentenceInformation.get("sentenceTime").toString(),
+                sentenceInformation.get("isChanged").toString());
     }
 }
 
