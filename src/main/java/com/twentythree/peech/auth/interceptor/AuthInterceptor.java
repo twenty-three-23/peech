8652427d04b436be0b59e0ab6636c8010f8e36da
snacktime <tth8181@gmail.com>
2024-07-11 13:41:21 +0900

    feat:  TT-223 log 수집
    AuthInterceptor를 통해 userId를 request에 담고,
    RequestLogIntreceptor를 통해서 userId, path, uuid를 출력하고 logs/ 밑에 로그를 파일로 저장한다.

---
+++ src/main/java/com/twentythree/peech/auth/interceptor/AuthInterceptor.java
@@ -0,0 +1,59 @@
+package com.twentythree.peech.auth.interceptor;
+
+import com.twentythree.peech.common.exception.UserAlreadyExistException;
+import com.twentythree.peech.common.utils.JWTUtils;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final JWTUtils jwtUtils;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        try {
+
+            final String BEARER = "Bearer ";
+
+            String token = request.getHeader("Authorization");
+
+            if (token.isEmpty()) {
+                throw new UserAlreadyExistException("로그인을 다시 해주세요");
+            }
+
+            String credential;
+
+            if (token.startsWith(BEARER)) {
+                credential = token.substring(BEARER.length());
+            } else {
+                throw new IllegalArgumentException("token의 type이 올바르지 않습니다.");
+            }
+
+            Long userId = Long.parseLong(jwtUtils.parseJWT(credential).getPayload().get("userId").toString());
+
+            if (userId == null) {
+                throw new IllegalArgumentException("가입이 안된 유저 입니다.");
+            }
+
+            request.setAttribute("userId", userId);
+            return true;
+        } catch (JwtException e) {
+            throw e;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
