359c79c91f24ed6fcfbaf325989a1eadd6336a4b
ch8930 <ch8930@naver.com>
2024-07-16 03:45:39 +0900

    !bug: TT-259 클라이언트로부터 받은 파일 입력이 처리안되는 오류 해결
    음성파일을 받은 naming 불일치로 인한 문제였으며 서버쪽에서 name을 변경하며 해결함

--- src/main/java/com/twentythree/peech/script/stt/dto/request/STTRequestDto.java
+++ src/main/java/com/twentythree/peech/script/stt/dto/request/STTRequestDto.java
@@ -3,5 +3,5 @@ package com.twentythree.peech.script.stt.dto.request;
 import org.springframework.web.multipart.MultipartFile;
 
 public record STTRequestDto(
-        MultipartFile media
+        MultipartFile file
 ) { }
