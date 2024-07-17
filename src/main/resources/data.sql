1d3ad94d551d6128706ed58607b671a4175628e1
snacktime <tth8181@gmail.com>
2024-07-17 15:29:05 +0900

    fix: TT-273 테스트 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,87 @@
 INSERT INTO user (user_id, device_id, created_at, updated_at) VALUES
-                                                                  (1, 'device_1', '2018-02-01', '2018-12-18');
+    (1, 'device_1', '2018-02-01', '2018-12-18');
 
-INSERT INTO theme (theme_id, user_id, created_at, updated_at, theme_title) VALUES
-                                                                                     (1, 1, '2022-11-08', '2021-02-24', 'Package Title 1'),
-                                                                                     (2, 1, '2023-03-03', null, 'Package Title 2');
-INSERT INTO version (major_version, minor_version, theme_id, created_at) VALUES
-                                                                               (1, 0, 1, '2021-01-08'),
-                                                                               (1, 1, 1, '2021-01-11'),
-                                                                               (1, 2, 1, '2021-01-11'),
-                                                                               (2, 0, 1, '2021-02-03'),
-                                                                               (3, 0, 1, '2021-02-24');
+INSERT INTO usage_time(usage_time_id, user_id, created_at, updated_at, remaining_time) VALUES
+    (1, 1, '2018-02-01', '2018-12-18', '9000');
 
-INSERT INTO script (script_id, script_content, created_at, d_type, major_version, minor_version, total_expect_time) VALUES
-                                                                                                     (1, 'Script Content 1', '2022-05-22', 1, 1, 0, '00:01:30'),
-                                                                                                     (2, 'Script Content 2', '2022-05-22', 1, 2, 0, '00:01:30'),
-                                                                                                     (3, 'Script Voice Content 1.1', '2022-05-22', 1, 1, 1, '00:01:30'),
-                                                                                                     (4, 'Script Voice Content 1.2', '2022-05-22', 1, 1, 2, '00:01:30');
+INSERT INTO theme (theme_id, theme_title, user_id, updated_at, created_at) VALUES
+    (1, 'Theme Title 1', 1, '2018-02-01', '2018-12-18'),
+    (2, 'Theme Title 2', 1, '2018-02-01', '2018-12-18'),
+    (3, 'Theme Title 3', 1, '2018-02-01', '2018-12-18'),
+    (4, 'Theme Title 4', 1, '2018-02-01', '2018-12-18'),
+    (5, 'Theme Title 5', 1, '2018-02-01', '2018-12-18'),
+    (6, 'Theme Title 6', 1, '2018-02-01', '2018-12-18'),
+    (7, 'Theme Title 7', 1, '2018-02-01', '2018-12-18'),
+    (8, 'Theme Title 8', 1, '2018-02-01', '2018-12-18'),
+    (9, 'Theme Title 9', 1, '2018-02-01', '2018-12-18'),
+    (10, 'Theme Title 10', 1, '2018-02-01', '2018-12-18');
+
+INSERT INTO version (theme_id, major_version, minor_version, created_at) VALUES
+    (1, 1, 0, '2018-02-01'),
+    (1, 1, 1, '2018-02-01'),
+    (1, 1, 2, '2018-02-01'),
+    (1, 1, 3, '2018-02-01'),
+    (1, 2, 0, '2018-02-01'),
+    (1, 2, 1, '2018-02-01'),
+    (2, 1, 0, '2018-02-01'),
+    (2, 1, 1, '2018-02-01'),
+    (2, 2, 0, '2018-02-01'),
+    (2, 2, 1, '2018-02-01');
+
+
+INSERT INTO script (script_id, script_content, theme_id, created_at, d_type, major_version, minor_version, total_expect_time) VALUES
+    (1, 'Hello. my 1. name is 1', 1,'2018-02-01', 'INPUT', 1, 0, '00:01:30'),
+    (5, 'Hello. my 5. name is 5', 1,'2018-02-01', 'INPUT', 2, 0, '00:01:30'),
+    (7, 'Hello. my 7. name is 7', 2,'2018-02-01', 'INPUT', 1, 0, '00:01:30'),
+    (10, 'Hello. my 10. name is 10', 2,'2018-02-01', 'INPUT', 2, 0, '00:01:30');
+
+INSERT INTO script (script_id, script_content, theme_id, created_at, d_type, major_version, minor_version, total_real_time) VALUES
+    (2, 'Hello. my 2. name is 2', 1,'2018-02-01', 'STT', 1, 1, '00:01:30'),
+    (3, 'Hello. my 3. name is 3', 1,'2018-02-01', 'STT', 1, 2, '00:01:30'),
+    (4, 'Hello. my 4. name is 4', 1,'2018-02-01', 'STT', 1, 3, '00:01:30'),
+    (6, 'Hello. my 6. name is 6', 1,'2018-02-01', 'STT', 2, 1, '00:01:30'),
+    (8, 'Hello. my 8. name is 8', 2,'2018-02-01', 'STT', 1, 1, '00:01:30'),
+    (9, 'Hello. my 9. name is 9', 2,'2018-02-01', 'STT', 2, 1, '00:01:30');
+
 
 INSERT INTO sentence (sentence_id, sentence_content, sentence_order, paragraph_id, script_id, created_at, sentence_expect_time) VALUES
-                                                                                                              (1, 'Sentence Content 1', 4, 1, 1, '2022-05-07','00:01:40'),
-                                                                                                              (2, 'Sentence Content 2', 8, 1, 1, '2023-11-25','00:01:39'),
-                                                                                                              (3, 'Sentence Content 3', 10, 2, 1, '2020-05-09','00:01:38'),
-                                                                                                              (4, 'Sentence Content 4', 9, 2, 1, '2020-12-06','00:01:37'),
-                                                                                                              (5, 'Sentence Content 5', 6, 3, 1, '2021-06-21','00:01:36'),
-                                                                                                              (6, 'Sentence Content 6', 1, 3, 1, '2018-12-27','00:01:35'),
-                                                                                                              (7, 'Sentence Content 7', 5, 4, 1, '2020-04-25','00:01:34'),
-                                                                                                              (8, 'Sentence Content 8', 2, 4, 1, '2020-04-08','00:01:33'),
-                                                                                                              (9, 'Sentence Content 9', 3, 0, 1, '2018-04-22','00:01:32'),
-                                                                                                              (10, 'Sentence Content 10', 7, 0, 1, '2019-01-14','00:01:31'),
-                                                                                                              (11, 'Sentence Content 11', 1, 1, 1, '2019-01-14','00:01:31');
+    (1, 'Hello.', 1, 1, 1, '2018-02-01', '00:01:30'),
+    (3, 'my 1.', 2, 1, 1, '2018-02-01', '00:01:30'),
+    (2, 'name is 1', 3, 2, 1, '2018-02-01', '00:01:30'),
+
+    (4, 'Hello.', 1, 1, 2, '2018-02-01', '00:01:30'),
+    (5, 'my 2.', 2, 1, 2, '2018-02-01', '00:01:30'),
+    (6, 'name is 2', 3, 2, 2, '2018-02-01', '00:01:30'),
+
+    (7, 'Hello.', 1, 1, 3, '2018-02-01', '00:01:30'),
+    (8, 'my 3.', 2, 1, 3, '2018-02-01', '00:01:30'),
+    (9, 'name is 3', 3, 2, 3, '2018-02-01', '00:01:30'),
+
+    (10, 'Hello.', 1, 1, 4, '2018-02-01', '00:01:30'),
+    (11, 'my 4.', 2, 1, 4, '2018-02-01', '00:01:30'),
+    (12, 'name is 4', 3, 2, 5, '2018-02-01', '00:01:30'),
+
+    (13, 'Hello.', 1, 1, 5, '2018-02-01', '00:01:30'),
+    (14, 'my 5.', 2, 1, 5, '2018-02-01', '00:01:30'),
+    (15, 'name is 5', 3, 2, 5, '2018-02-01', '00:01:30'),
+
+    (16, 'Hello.', 1, 1, 6, '2018-02-01', '00:01:30'),
+    (17, 'my 6.', 2, 1, 6, '2018-02-01', '00:01:30'),
+    (18, 'name is 6', 3, 2, 6, '2018-02-01', '00:01:30'),
+
+    (19, 'Hello.', 1, 1, 7, '2018-02-01', '00:01:30'),
+    (20, 'my 7.', 2, 1, 7, '2018-02-01', '00:01:30'),
+    (21, 'name is 7', 3, 2, 7, '2018-02-01', '00:01:30'),
+
+    (22, 'Hello.', 1, 1, 8, '2018-02-01', '00:01:30'),
+    (23, 'my 8.', 2, 1, 8, '2018-02-01', '00:01:30'),
+    (24, 'name is 8', 3, 2, 8, '2018-02-01', '00:01:30'),
+
+    (25, 'Hello.', 1, 1, 9, '2018-02-01', '00:01:30'),
+    (26, 'my 9.', 2, 1, 9, '2018-02-01', '00:01:30'),
+    (27, 'name is 9', 3, 2, 9, '2018-02-01', '00:01:30'),
+
+    (28, 'Hello.', 1, 1, 10, '2018-02-01', '00:01:30'),
+    (29, 'my 10.', 2, 1, 10, '2018-02-01', '00:01:30'),
+    (30, 'name is 10', 3, 2, 10, '2018-02-01', '00:01:30');
+
