5f8958a55f6f663cb938b37b0101ee5d49a5b42a
ch8930 <ch8930@naver.com>
2024-07-19 02:27:38 +0900

    fix: TT-258 stt dto 수정
    - 전체 소요시간 response dto에 추가

--- src/main/java/com/twentythree/peech/script/stt/dto/response/STTScriptResponseDTO.java
+++ src/main/java/com/twentythree/peech/script/stt/dto/response/STTScriptResponseDTO.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalTime;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class STTScriptResponseDTO {
+
+    LocalTime totalRealTime;
     List<STTParagraphDTO> script;
 }
