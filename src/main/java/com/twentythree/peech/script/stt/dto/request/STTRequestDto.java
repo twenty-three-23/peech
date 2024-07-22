9694c1b176f0a9d2b3255a74ccb2981a99338c49
ch8930 <ch8930@naver.com>
2024-07-22 16:08:52 +0900

    fix: TT-268 stt request dto 수정
    - 프론트로부터 계산된 시간을 기반으로 사용자가 사용할 수 있는 시간 차감하도록 임시 변경
    
    - 안쓰는 script 팩토리 메소드 삭제

--- src/main/java/com/twentythree/peech/script/stt/dto/request/STTRequestDto.java
+++ src/main/java/com/twentythree/peech/script/stt/dto/request/STTRequestDto.java
@@ -3,5 +3,6 @@ package com.twentythree.peech.script.stt.dto.request;
 import org.springframework.web.multipart.MultipartFile;
 
 public record STTRequestDto(
-        MultipartFile file
+        MultipartFile file,
+        Long time
 ) { }
