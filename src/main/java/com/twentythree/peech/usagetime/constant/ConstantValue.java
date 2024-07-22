81c3b8f6bc423b8b38fe5f31eed8a2ce93d52c35
snacktime <tth8181@gmail.com>
2024-07-22 19:04:06 +0900

    fix: TT-299 녹음 가능 최대 시간 수정
    5초 정도의 여유를 둠

--- src/main/java/com/twentythree/peech/usagetime/constant/ConstantValue.java
+++ src/main/java/com/twentythree/peech/usagetime/constant/ConstantValue.java
@@ -5,5 +5,5 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ConstantValue {
-    public static final Long MAX_AUDIO_TIME = 600L;
+    public static final Long MAX_AUDIO_TIME = 600L + 5L;
 }
