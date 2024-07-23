ace915d94c3ef932ec26fd156d42e3a979ec3680
snacktime <tth8181@gmail.com>
2024-07-23 13:07:33 +0900

    fix: TT-299 최대 녹음 가능 시간 15분으로 수정
    최대 녹음 가능 시간을 600초(10분) 에서 900초(15분)으로 수정
    여유 시간을 상수로 빼서 사용

--- src/main/java/com/twentythree/peech/usagetime/constant/ConstantValue.java
+++ src/main/java/com/twentythree/peech/usagetime/constant/ConstantValue.java
@@ -5,5 +5,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ConstantValue {
-    public static final Long MAX_AUDIO_TIME = 600L + 5L;
+    public static final Long MAX_AUDIO_TIME = 600L;
+    public static final Long BUFFER_TIME = 5L;
 }
