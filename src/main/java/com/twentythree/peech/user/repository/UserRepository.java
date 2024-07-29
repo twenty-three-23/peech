53975c810eb7b2068ddfc6a18859e436995253ab
snacktime <tth8181@gmail.com>
2024-07-29 16:10:04 +0900

    feat: TT-328 유저 aggregate 저장
    user aggregate를 user 와 usageTime entity로 변환하여 저장

--- src/main/java/com/twentythree/peech/user/repository/UserRepository.java
+++ src/main/java/com/twentythree/peech/user/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.twentythree.peech.user.repository;
 
-import com.twentythree.peech.user.domain.UserEntity;
+import com.twentythree.peech.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByDeviceId(String deviceId);
+
+    Optional<UserEntity> findByNickName(String nickName);
 }
