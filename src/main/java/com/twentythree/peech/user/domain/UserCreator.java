cd6f4aeac50fe00aa36e263211c014c216ea608c
snacktime <tth8181@gmail.com>
2024-07-30 23:15:53 +0900

    feat: TT-328 유저에서 필요한 정보를 채워서 완성하는 도메인
    닉네임 중복을 학인, 유저 상태를 확인

--- src/main/java/com/twentythree/peech/user/domain/UserCreator.java
+++ src/main/java/com/twentythree/peech/user/domain/UserCreator.java
@@ -1,11 +1,9 @@
 package com.twentythree.peech.user.domain;
 
 import com.twentythree.peech.usagetime.constant.UsageConstantValue;
-import com.twentythree.peech.user.AuthorizationIdentifier;
-import com.twentythree.peech.user.SignUpFinished;
-import com.twentythree.peech.user.UserRole;
+import com.twentythree.peech.user.entity.AuthorizationIdentifier;
+import com.twentythree.peech.user.value.*;
 import com.twentythree.peech.user.validator.UserValidator;
-import com.twentythree.peech.user.UserGender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -28,5 +26,18 @@ public class UserCreator {
 
     public UserDomain createUserByEmail(AuthorizationIdentifier authorizationIdentifier, String email, SignUpFinished signUpFinished) {
         return UserDomain.ofEmail(authorizationIdentifier, email, UserRole.ROLE_COMMON, UsageConstantValue.DEFAULT_USAGE_TIME, UsageConstantValue.DEFAULT_USAGE_TIME, signUpFinished);
+    }
+
+    public UserDomain completeUser(UserDomain userDomain,  AuthorizationIdentifier authorizationIdentifier, String firstName, String lastName, LocalDate birth, UserGender gender, String email, String nickName, UserRole role, UserStatus userStatus, SignUpFinished signUpFinished, LocalDate deleteAt) {
+        if (userValidator.NickNameIsNotDuplicated(nickName) && userDomain.getUserStatus() != UserStatus.DELETE && userStatus != UserStatus.DELETE) {
+            userDomain.setUser(authorizationIdentifier, firstName, lastName, birth, gender, email, nickName, role, userStatus, signUpFinished, deleteAt);
+        } else {
+            if (userDomain.getUserStatus() == UserStatus.DELETE || userStatus == UserStatus.DELETE) {
+                throw new IllegalArgumentException("삭제된 유저는 정보를 변경 할 수 없습니다.");
+            } else {
+                throw new IllegalArgumentException("닉네임이 중복 되었습니다.");
+            }
+        }
+        return userDomain;
     }
 }
