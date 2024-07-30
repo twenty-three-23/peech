9c40de9d77a15a3c1bbe3f688fa849705246c678
snacktime <tth8181@gmail.com>
2024-07-30 10:11:28 +0900

    feat: TT-329 유저 삭제 endpoint 생성
    api/v2/user로 delete 메소드를 보내면 유저를 삭제
    유저 아이디는 accesstoken에서 가져와야 함으로 후에 추가

--- src/main/java/com/twentythree/peech/user/controller/UserController.java
+++ src/main/java/com/twentythree/peech/user/controller/UserController.java
@@ -1,21 +1,22 @@
 package com.twentythree.peech.user.controller;
 
+import com.twentythree.peech.user.domain.UserDomain;
+import com.twentythree.peech.user.domain.UserMapper;
 import com.twentythree.peech.user.dto.AccessAndRefreshToken;
 import com.twentythree.peech.user.dto.request.CreateUserRequestDTO;
+import com.twentythree.peech.user.dto.response.UserDeleteResponseDTO;
 import com.twentythree.peech.user.dto.response.UserIdTokenResponseDTO;
 import com.twentythree.peech.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class UserController implements SwaggerUserController{
 
     private final UserService userService;
+    private final UserMapper userMapper;
 
     @Operation(summary = "유저 가입",
             description = "deviceId를 RequestBody에 담아 요청하면 새로운 유저를 생성하고 생성된 UserId를 응답한다.")
@@ -43,5 +44,15 @@ public class UserController implements SwaggerUserController{
     public UserIdTokenResponseDTO reIssuanceUserToken(@RequestBody CreateUserRequestDTO request) {
         String token = userService.reIssuanceUserToken(request.getDeviceId());
         return new UserIdTokenResponseDTO(token, token);
+    }
+
+    @Operation(summary = "유저 삭제")
+    @DeleteMapping("api/v2/user")
+    public UserDeleteResponseDTO deleteUser() {
+        // TODO 유저 토큰에서 userId 가져오는 코드
+        Long userId = 123L; // 임시 유저
+        UserDomain userDomain = userService.deleteUser(userId);
+        userMapper.saveUserDomain(userDomain);
+        return new UserDeleteResponseDTO(userDomain.getDeleteAt());
     }
 }
