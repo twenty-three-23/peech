0606eafe85e08984b0d82ec12514093906e3e62d
snacktime <tth8181@gmail.com>
2024-08-13 15:47:08 +0900

    chore: TT-379 @LoginUserId deprecated
    arguement resolver를 써 userId를 가져오는 것들을 deprecated 시킴

--- src/main/java/com/twentythree/peech/auth/config/ArgumentResolverConfig.java
+++ src/main/java/com/twentythree/peech/auth/config/ArgumentResolverConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Deprecated
 @RequiredArgsConstructor
 @Configuration
 public class ArgumentResolverConfig implements WebMvcConfigurer {
