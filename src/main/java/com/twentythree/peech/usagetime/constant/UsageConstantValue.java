ac57fb6a3962de15e3cf092e86a0049c5057e81c
snacktime <tth8181@gmail.com>
2024-08-17 16:36:29 +0900

    fix: TT-382 max autio time 25분
    최대 녹음 시간을 25분으로 변경

--- src/main/java/com/twentythree/peech/usagetime/constant/UsageConstantValue.java
+++ src/main/java/com/twentythree/peech/usagetime/constant/UsageConstantValue.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class UsageConstantValue {
     public static final Long DEFAULT_USAGE_TIME = 9000L;
-    public static final Long MAX_AUDIO_TIME = 900L;
+    public static final Long MAX_AUDIO_TIME = 1500L;
     public static final Long BUFFER_TIME = 5L;
 }
