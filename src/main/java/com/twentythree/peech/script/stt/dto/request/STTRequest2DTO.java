86e14ec77143f57251aebdc967f09e7cf15d6ead
snacktime <tth8181@gmail.com>
2024-08-29 16:41:17 +0900

    feat: TT-402 web stt 로직

---
+++ src/main/java/com/twentythree/peech/script/stt/dto/request/STTRequest2DTO.java
@@ -0,0 +1,12 @@
+package com.twentythree.peech.script.stt.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class STTRequest2DTO {
+    private String file;
+}
