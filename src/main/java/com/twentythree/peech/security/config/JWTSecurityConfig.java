4ead687966ef3c2bec07302db7a6d8178987abd0
ch8930 <ch8930@naver.com>
2024-09-04 16:57:23 +0900

    fix: 필요없는 로직 제거
    - JWTProvider 사용하지 않는 로직이라 삭제

--- src/main/java/com/twentythree/peech/security/config/JWTSecurityConfig.java
+++ src/main/java/com/twentythree/peech/security/config/JWTSecurityConfig.java
@@ -3,7 +3,6 @@ package com.twentythree.peech.security.config;
 import com.twentythree.peech.common.utils.JWTUtils;
 import com.twentythree.peech.security.filter.JWTAuthenticationFilter;
 import com.twentythree.peech.security.handler.JWTAuthAccessDeniedHandler;
-import com.twentythree.peech.security.jwt.JWTAuthenticationProvider;
 import com.twentythree.peech.security.jwt.JWTUserDetailsService;
 import com.twentythree.peech.user.service.UserService;
 import org.springframework.context.annotation.Bean;
@@ -25,11 +24,6 @@ public class JWTSecurityConfig {
     @Bean
     public JWTAuthenticationFilter jwtAuthenticationFilter() {
         return new JWTAuthenticationFilter(jwtUtils, jwtUserDetailsService);
-    }
-
-    @Bean
-    public JWTAuthenticationProvider jwtAuthenticationProvider() {
-        return new JWTAuthenticationProvider(userService);
     }
 
     @Bean
