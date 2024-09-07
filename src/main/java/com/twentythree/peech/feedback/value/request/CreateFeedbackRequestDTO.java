9707201d20a57d911b14f3a7ae5953ba3f23d4b5
snacktime <tth8181@gmail.com>
2024-09-07 14:49:52 +0900

    feat: TT-415 feedback 생성 endpoint 생성
    feedback 생성을 위한 endpoint 개방

---
+++ src/main/java/com/twentythree/peech/feedback/value/request/CreateFeedbackRequestDTO.java
@@ -0,0 +1,12 @@
+package com.twentythree.peech.feedback.value.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateFeedbackRequestDTO {
+    private String message;
+}
