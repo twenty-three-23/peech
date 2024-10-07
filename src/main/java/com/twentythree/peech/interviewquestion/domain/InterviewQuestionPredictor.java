79a62d10c426f77b8504202e3acf283d4a63f170
snacktime <tth8181@gmail.com>
2024-10-07 16:23:55 +0900

    feat: TT-454
    자기소개 만으로 면접질문 생성

--- src/main/java/com/twentythree/peech/interviewquestion/domain/InterviewQuestionPredictor.java
+++ src/main/java/com/twentythree/peech/interviewquestion/domain/InterviewQuestionPredictor.java
@@ -23,8 +23,15 @@ public class InterviewQuestionPredictor {
     public InterviewQuestionDomain predictInterviewQuestion(InterviewQuestionDomain interviewQuestionDomain) {
         String scriptContent = interviewQuestionDomain.getScriptContent();
 
-        String prompt = "아래의 글은 자기소개글 입니다. 이 자기소개를 토대로 수준 높은 질문을 4개를 만들어서 [\n" +
-                "\"면접질문 내용\", \"면접질문 내용\"] 이런형식으로 만들어줘 \n"  + scriptContent;
+        String prompt = "아래의 글은 회사 면접을 위한 자기소개 글입니다\n" +
+                "\n" +
+                "면접자의 자기소개에 언급된 경험을 바탕으로 회사에서 어떻게 역량을 발휘할 수 있을지 물어보는 질문을 2개를 만들어야 합니다.\n" +
+                "추가로 면접자의 자기소개에 언급된 내용을 바탕으로 회사에서 어떤게 응용할 수 있을지 물어보는 질문을 2개 추가로 만들어 모두 합쳐 4개의 면접 질문을 주세요\n" +
+                "\n" +
+                "답변 형식은 무조건 다른 어떤한 것도 추가하지 말고 [\"면접질문 내용\", \"면접질문 내용\"] 의 형식으로 만들어야 합니다.\n" +
+                "\n" +
+                "자기소개:\n" +
+                scriptContent;
 
         GPTRequest gptRequest = new GPTRequest("gpt-4o-mini", prompt);
         GPTResponse gptResponse = interviewQuestionPredictorGPT.predictQuestion(gptRequest);
